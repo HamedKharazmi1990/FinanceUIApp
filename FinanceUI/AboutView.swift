c91b065dcb10c727b91b996509d04b8929b4adbe
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-08 22:46:53 +0100

    Implemented About View

--- FinanceUI/AboutView.swift
+++ FinanceUI/AboutView.swift
@@ -17,6 +17,7 @@ struct AboutView: View {
                 
                 AvatarView()
                 
+                ChoicesView()
                 
                 Spacer()
             }
@@ -43,12 +44,12 @@ struct AvatarView: View {
         VStack(alignment: .leading) {
             Image(systemName: "person.crop.circle")
                 .resizable()
-                .frame(width: 50, height: 50)
+                .frame(width: 30, height: 30)
                 .foregroundColor(fontColor)
                 .background(backgroundX)
                 .clipShape(Circle())
                 
-            TextView(text: "Hamed Kharazmi", font: .largeTitle)
+            TextView(text: "Hamed Kharazmi", font: .title)
             TextView(text: "Software Engineer", font: .headline)
         }.padding()
     }
@@ -62,6 +63,52 @@ struct TextView: View {
         Text(text)
             .foregroundColor(fontColor)
             .font(font)
+    }
+}
+
+struct ChoicesView: View {
+    
+    let dataArray: [(String, String, String?)] = [
+        (title: "Corporate App", systemImage: "building.2", description: nil),
+        (title: "Security Settings", systemImage: "lock", description: nil),
+        (title: "Online Shopping", systemImage: "cart", description: "Add to cart"),
+        (title: "Groceries", systemImage: "list.bullet.rectangle", description: nil),
+        (title: "Utilities", systemImage: "wrench.and.screwdriver", description: nil),
+        (title: "Thumb Scanner", systemImage: "hand.thumbsup", description: "Scan thumb for identification")
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            ForEach(0..<dataArray.count, id: \.self) { i in
+                SingleChoiceView(text: dataArray[i].0, systemImage: dataArray[i].1, description: dataArray[i].2)
+            }
+        }
+    }
+}
+
+struct SingleChoiceView: View {
+    let text: String
+    let systemImage: String
+    let description: String?
+    
+    var body: some View {
+        Label(text, systemImage: systemImage)
+            .font(.headline)
+            .foregroundStyle(fontColor)
+            .padding(.horizontal)
+            .labelStyle(SpacedLabelStyle())
+            .symbolRenderingMode(.hierarchical)
+            .accessibilityLabel(description ?? text)
+    }
+}
+
+struct SpacedLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(alignment: .center, spacing: 20) {
+            configuration.icon
+                .imageScale(.large)
+            configuration.title
+        }
     }
 }
 
