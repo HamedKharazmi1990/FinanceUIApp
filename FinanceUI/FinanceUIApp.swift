215f1a3d2dbd8d445a925e2991a61dcf1a4a5544
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-08 19:46:39 +0100

    Initial Commit

---
+++ FinanceUI/FinanceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceUIApp.swift
+//  FinanceUI
+//
+//  Created by Hamed Kharazmi on 08.01.26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
